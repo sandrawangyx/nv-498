37b4212964bce2c47fc5b700697524853e35b0d7
sandra <yx97.wang@gmail.com>
2018-07-23 09:29:27 -0400

    no msg

--- 498Final/src/main/resources/templates/js/ageGroupBar.js
+++ 498Final/src/main/resources/templates/js/ageGroupBar.js
@@ -1,111 +1,47 @@
-var margin = {top: 20, right: 40, bottom: 30, left: 20},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom,
-    barWidth = Math.floor(width / 19) - 1;
+var svg = d3.select("svg"),
+    margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom;
 
-var x = d3.scale.linear()
-    .rangeRound([barWidth / 2, width - barWidth / 2]);
+var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+    y = d3.scaleLinear().rangeRound([height, 0]);
 
-var y = d3.scale.linear()
-    .range([height, 0]);
-
-var xAxis = d3.svg.axis().scale(x).orient("bottom")
-.ticks(d3.timeYear)
-.tickFormat(d3.format("d"));
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("right")
-    .tickSize(-width)
-    .tickFormat(function(d) {
-    var formatSuffixDecimal2 = d3.format(".2s");
-    return formatSuffixDecimal2(d); });
-
-// An SVG element with a bottom-right origin.
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
+var g = svg.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// A sliding container to hold the bars by year.
-var years = svg.append("g")
-    .attr("class", "years");
+function filterCriteria(d) {
+    return d.year === "1971";
+}
+d3.csv("CSV/sexageGroup_part.csv", function(d) {
+  d.value = +d.value;
+  d.AgeGroup = d.AgeGroup.substring(0, d.AgeGroup.length - 6);
+  return d;
+}, function(error, data) {
+  if (error) throw error;
 
-// A label for the current year.
-var title = svg.append("text")
-    .attr("class", "title")
-    .attr("dy", ".71em")
-    .text("Population by Age Group");
+  x.domain(data.map(function(d) { return d.AgeGroup; }));
+  y.domain([0, d3.max(data, function(d) { return d.value; })]);
 
- var dateFormat = d3.time.format("%Y");
+  g.append("g")
+      .attr("class", "axis axis--x")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
 
-d3.csv("CSV/sexageGroup_part.csv", function(data) {
-  // Convert strings to numbers.
-    //data.year= parse(data.year);
-    data.year = +data.year;
-    data.value= +data.value;
+  g.append("g")
+      .attr("class", "axis axis--y")
+      .call(d3.axisLeft(y).tickFormat(d3.format(".2f")))
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.71em")
+      .attr("text-anchor", "end");
 
-  // Compute the extent of the data set in age and years.
-  var year0 = d3.min(data, function(d) { return d.year; }),
-      year1 = d3.max(data, function(d) { return d.year; }),
-      year = year1;
-
-   var minValue = d3.min(data, function(d) {return d.value}),
-   maxValue= d3.max(data, function(d){return d.value});
-
-  // Update the scale domains.
-  x.domain([year0, year1]);
-  console.log(maxValue);
-  y.domain([0, maxValue]);//(data, function(d) { return d.value; })]);
-
-
-  // Produce a map from year and value to different age group.
-  data = d3.nest()
-      .key(function(d) { return d.year; })
-      .key(function(d) { return d.AgeGroup; })
-      .rollup(function(v) { return v.map(function(d) { return d.value; }); })
-      .map(data);
-svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")").call(xAxis);
-
-  // Add an axis to show the population values.
-  svg.append("g")
-      .attr("class", "y axis")
-      .attr("transform", "translate(" + width + ",0)")
-      .call(yAxis)
-    .selectAll("g")
-    .filter(function(value) { return !value; })
-      .classed("zero", true);
-
-  // Add labeled rects for each year (so that no enter or exit is required).
-  var dataYear = years.selectAll(".dataYear")
-  .data(d3.range(year0, year1, 1)).enter().append("g").attr("class","dataYear")
-  .attr("transform", function(dataYear){return "translate(" +x(dataYear) + ",0)";});
-
-  // Add labels to show age (separate; not animated).
-  /*svg.selectAll(".age")
-      .data(d3.range(0, year + 1, 5))
-    .enter().append("text")
-      .attr("class", "age")
-      .attr("x", function(age) { return x(year - age); })
-      .attr("y", height + 4)
-      .attr("dy", ".71em")
-      .text(function(age) { return age; });
-*/
-  // Allow the arrow keys to change the displayed year.
-  window.focus();
-  d3.select(window).on("keydown", function() {
-    switch (d3.event.keyCode) {
-      case 37: year = Math.max(year0, year - 10); break;
-      case 39: year = Math.min(year1, year + 10); break;
-    }
-    update();
-  });
-
-  function update() {
-    if (!(year in data)) return;
-    title.text(year);
-
-
-  }
-  });
+  g.selectAll(".bar")
+    .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.AgeGroup); })
+      .attr("y", function(d) { return y(d.value); })
+      .attr("width", x.bandwidth())
+      .attr("height", function(d) { return height - y(d.value); });
+});
