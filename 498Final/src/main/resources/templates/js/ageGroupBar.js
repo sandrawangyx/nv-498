dfb1e58f9a6942c74b5bd37436f0d4c59e5f259e
sandra <yx97.wang@gmail.com>
2018-07-23 09:54:31 -0400

    x-substring, x,y labels, y show full text

--- 498Final/src/main/resources/templates/js/ageGroupBar.js
+++ 498Final/src/main/resources/templates/js/ageGroupBar.js
@@ -1,5 +1,5 @@
 var svg = d3.select("svg"),
-    margin = {top: 20, right: 20, bottom: 30, left: 40},
+    margin = {top: 20, right: 20, bottom: 50, left: 70},
     width = +svg.attr("width") - margin.left - margin.right,
     height = +svg.attr("height") - margin.top - margin.bottom;
 
@@ -20,21 +20,28 @@ d3.csv("CSV/sexageGroup_part.csv", function(d) {
   if (error) throw error;
 
   x.domain(data.map(function(d) { return d.AgeGroup; }));
-  y.domain([0, d3.max(data, function(d) { return d.value; })]);
+  y.domain([0, d3.max(data, function(d) { return d.value; }) + 5000]);
 
   g.append("g")
       .attr("class", "axis axis--x")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x));
 
+  g.append("text")
+   .attr("transform","translate(" + (width/2) + " ," + (height + margin.top + 10) + ")")
+            .style("text-anchor", "middle")
+            .text("Age Group");
+
+
   g.append("g")
       .attr("class", "axis axis--y")
-      .call(d3.axisLeft(y).tickFormat(d3.format(".2f")))
-    .append("text")
+      .call(d3.axisLeft(y).tickFormat(d3.format(".0s")));
+    g.append("text")
       .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", "0.71em")
-      .attr("text-anchor", "end");
+      .attr("y", 30 - margin.left)
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle").text("Population");
 
   g.selectAll(".bar")
     .data(data)
